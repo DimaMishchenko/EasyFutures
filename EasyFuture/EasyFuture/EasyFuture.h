4d555c528a2c5240835c3aa37ecc5401c56ff9c4
Dima Mishchenko <narmdv5@gmail.com>
2018-06-30 14:48:09 +0300

    Init project.

---
+++ EasyFuture/EasyFuture/EasyFuture.h
@@ -0,0 +1,19 @@
+//
+//  EasyFuture.h
+//  EasyFuture
+//
+//  Created by Dima Mishchenko on 30.06.2018.
+//  Copyright © 2018 Dima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyFuture.
+FOUNDATION_EXPORT double EasyFutureVersionNumber;
+
+//! Project version string for EasyFuture.
+FOUNDATION_EXPORT const unsigned char EasyFutureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyFuture/PublicHeader.h>
+
+
